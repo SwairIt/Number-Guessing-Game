f9a4df1bc2335bb2366573da00be0e4b48f235c4
SwaitIt <1q2w3e4r5t6y7u89o0pi@gmail.com>
2025-05-14 16:45:00 +0300

    Вторая версия проекта: заменены несколько циклов while на один цикл for

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-from time import process_time
 
 random_number = random.randint(1, 101)
 print(random_number)
@@ -18,63 +17,28 @@ attempts = None
 
 if start == 1:
     attempts = 10
-    print("Отлично! Ты выбрал легкий уровень сложности.\n"
-            "Давай начнем игру!")
-    while attempts is not None and attempts != 0:
-        guess = int(input("Введи своё предположение\n>>>"))
-        if attempts == 1 and guess != random_number:
-            print(f"Ты не угадал. Число было {random_number}\nПопробуй еще раз!")
-        else:
-            if guess == random_number:
-                winner_attempt = 11 - attempts
-                attempts = 1
-                print(f"Поздравляю! Ты угадал правильное число с {winner_attempt} попытки")
-            else:
-                print("Неверно!")
-                if guess > random_number:
-                    print(f"Число меньше {guess}")
-                else:
-                    print(f"Число больше {guess}")
-        attempts -= 1
 elif start == 2:
     attempts = 5
-    print("Отлично! Ты выбрал средний уровень сложности.\n"
-            "Давай начнем игру!")
-    while attempts is not None and attempts != 0:
-        guess = int(input("Введи своё предположение\n>>>"))
-        if attempts == 1 and guess != random_number:
-            print(f"Ты не угадал. Число было {random_number}\nПопробуй еще раз!")
-        else:
-            if guess == random_number:
-                winner_attempt = 6 - attempts
-                attempts = 1
-                print(f"Поздравляю! Ты угадал правильное число с {winner_attempt} попытки")
-            else:
-                print("Неверно!")
-                if guess > random_number:
-                    print(f"Число меньше {guess}")
-                else:
-                    print(f"Число больше {guess}")
-        attempts -= 1
 elif start == 3:
     attempts = 3
-    print("Отлично! Ты выбрал сложный уровень сложности.\n"
-            "Давай начнем игру!")
-    while attempts is not None and attempts != 0:
-        guess = int(input("Введи своё предположение\n>>>"))
-        if attempts == 1 and guess != random_number:
-            print(f"Ты не угадал. Число было {random_number}\nПопробуй еще раз!")
-        else:
-            if guess == random_number:
-                winner_attempt = 4 - attempts
-                attempts = 1
-                print(f"Поздравляю! Ты угадал правильное число с {winner_attempt} попытки")
-            else:
-                print("Неверно!")
-                if guess > random_number:
-                    print(f"Число меньше {guess}")
-                else:
-                    print(f"Число больше {guess}")
-        attempts -= 1
 else:
     print("Нужно ввести число! Перезапусти программу.")
+
+
+for i in range(attempts):
+    guess = int(input("Введи своё предположение\n>>>"))
+    if attempts == 1 and guess != random_number:
+        print(f"Ты не угадал. Число было {random_number}\nПопробуй еще раз!")
+    else:
+        if guess == random_number:
+            attempts = 1
+            winner_attempt = i + 1
+            print(f"Поздравляю! Ты угадал правильное число с {winner_attempt} попытки")
+            break
+        else:
+            print("Неверно!")
+            if guess > random_number:
+                print(f"Число меньше {guess}")
+            else:
+                print(f"Число больше {guess}")
+    attempts -= 1
